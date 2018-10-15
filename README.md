2504aa2becadd36c9c955e611414b822abe24855
Danimal <runiondd@gmail.com>
2018-10-14 21:32:48 -0400

    just started. background, some jquery to move buttons on click.

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # unit-4-game
+
+Objects:
+
+Fighter
+    properties:
+        - Health points
+        - counter attack power
+        - alive/dead (visible)
+
+Defender
+        - Health points
+        - attach power
+        - alive/dead (visible)
+
+Game
+    Properties:
+        - hasWon = boolean
+    Methods:
+        - Choose Enemy
---
+++ assets/javascript/game.js
@@ -0,0 +1,28 @@
+var Game = {
+    hasWon: false, 
+
+    chooseEnemy : function () {
+
+    }
+
+}
+
+var Fighter = {
+    numHealthPoints: 0,
+    numCounterAttackPoints: 0,
+    isDead: false,
+
+    counterAttack : function() {   
+    
+    }
+}
+
+var Defender = {
+    numHealthPoints: 0,
+    numAttackPoints: 0,
+    isDead: false,
+    
+    attack : function() {
+
+    }
+}
